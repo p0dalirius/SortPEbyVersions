81e4e5db5407271bf6741595df13f50fe9971390
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-02-05 21:49:21 +0100

    Added sha256 hashes to versions dict

--- SortPEbyVersions.py
+++ SortPEbyVersions.py
@@ -10,8 +10,18 @@ import binascii
 import os
 import pefile
 import requests
+from rich import progress
 import time
 import shutil
+import hashlib
+
+
+def file_sha256sum(path_to_file):
+    sha256_hash = hashlib.sha256()
+    with open(path_to_file, "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
 
 
 def pe_get_version(pathtopefile):
@@ -22,46 +32,48 @@ def pe_get_version(pathtopefile):
     return data
 
 
-def download_pdb(pathtopefile, download_dir, debug=False):
+def download_pdb(download_dir, pathtopefile):
+    pdbname, guid, pdbage = get_pe_debug_infos(pathtopefile)
+    download_url = "http://msdl.microsoft.com/download/symbols/%s/%s%X/%s" % (pdbname, guid.upper(), pdbage, pdbname)
+    print("[>] Downloading %s" % download_url)
+    r = requests.head(
+        download_url,
+        headers={"User-Agent": "Microsoft-Symbol-Server/10.0.10036.206"},
+        allow_redirects=True
+    )
+    if r.status_code == 200:
+        target_file = download_dir + os.path.sep + pdbname
+        with progress.Progress() as p:
+            progress_bar, csize = p.add_task("[cyan]Downloading %s" % pdbname, total=int(r.headers["Content-Length"])), 1024*16
+            pdb = requests.get(r.url, headers={"User-Agent": "Microsoft-Symbol-Server/10.0.10036.206"}, stream=True)
+            with open(target_file, "wb") as f:
+                for chunk in pdb.iter_content(chunk_size=csize):
+                    f.write(chunk)
+                    p.update(progress_bar, advance=len(chunk))
+    else:
+        print("[!] (HTTP %d) Could not find %s " % (r.status_code, download_url))
+
+
+def get_pe_debug_infos(pathtopefile):
     p = pefile.PE(pathtopefile, fast_load=False)
     pedata = {d.name: d for d in p.OPTIONAL_HEADER.DATA_DIRECTORY}
     raw_debug_data = [e for e in p.parse_debug_directory(pedata["IMAGE_DIRECTORY_ENTRY_DEBUG"].VirtualAddress, pedata["IMAGE_DIRECTORY_ENTRY_DEBUG"].Size) if e.entry is not None]
     raw_debug_data = raw_debug_data[0].entry
-    guid = "%08X%04X%04X%s" % (raw_debug_data.Signature_Data1, raw_debug_data.Signature_Data2, raw_debug_data.Signature_Data3, binascii.hexlify(raw_debug_data.Signature_Data4).decode("utf-8").upper())
-    pdbname, guid, pdbage = raw_debug_data.PdbFileName.strip(b'\x00').decode("utf-8"), guid, raw_debug_data.Age
-    #
-    download_url = "http://msdl.microsoft.com/download/symbols/%s/%s%X/%s" % (pdbname, guid.upper(), pdbage, pdbname)
-    if debug:
-        print("[>] Downloading %s" % download_url)
-    retry = True
-    while retry:
-        try:
-            r = requests.head(download_url, headers={"User-Agent": "Microsoft-Symbol-Server/10.0.10036.206"}, allow_redirects=True)
-            if r.status_code == 200:
-                target_file = download_dir + os.path.sep + pdbname
 
-                csize = 1024*16
-                pdb = requests.get(r.url, headers={"User-Agent": "Microsoft-Symbol-Server/10.0.10036.206"}, stream=True)
-                with open(target_file, "wb") as f:
-                    for chunk in pdb.iter_content(chunk_size=csize):
-                        f.write(chunk)
-            else:
-                print("[!] (HTTP %d) Could not find %s " % (r.status_code, download_url))
-            retry = False
-        except Exception as e:
-            time.sleep(3)
-            print("Retrying")
-            retry = True
+    guid = "%08X%04X%04X%s" % (
+        raw_debug_data.Signature_Data1,
+        raw_debug_data.Signature_Data2,
+        raw_debug_data.Signature_Data3,
+        binascii.hexlify(raw_debug_data.Signature_Data4).decode("utf-8").upper()
+    )
+    return raw_debug_data.PdbFileName.strip(b'\x00').decode("utf-8"), guid, raw_debug_data.Age
 
 
 def parseArgs():
     parser = argparse.ArgumentParser(description="")
-
     parser.add_argument("-s", "--source-dir", default=None, required=True, help='')
     parser.add_argument("-a", "--archive-dir", default=None, required=True, help='')
-
     parser.add_argument("-v", "--verbose", default=False, action="store_true", help='Verbose mode. (default: False)')
-
     return parser.parse_args()
 
 
@@ -69,24 +81,34 @@ def process(path_to_file, versions, dump_dir, debug):
     try:
         pev = pe_get_version(path_to_file)
         filename = os.path.basename(path_to_file)
+        sha256hash = file_sha256sum(path_to_file)
 
         if filename not in versions.keys():
-            versions[filename] = []
+            versions[filename] = {}
+
+        if pev["FileVersion"] in versions[filename]:
+            if versions[filename][pev["FileVersion"]]["sha256"] != sha256hash:
+                print("[!] Hash mismatch for version %s" % pev["FileVersion"])
+                print("  | A: %s" % versions[filename][pev["FileVersion"]]["path"])
+                print("  | B: %s" % path_to_file)
 
         if pev["FileVersion"] not in versions[filename]:
-            versions[filename].append(pev["FileVersion"])
+            versions[filename][pev["FileVersion"]] = {
+                "path": path_to_file,
+                "sha256": sha256hash
+            }
             final_dir = dump_dir + "/%s/%s/" % (filename, pev["FileVersion"])
             if not os.path.exists(final_dir):
                 os.makedirs(final_dir, exist_ok=True)
             saved_filename = final_dir + os.path.sep + os.path.basename(filename)
             saved_filename = saved_filename.replace("//", "/")
-            #shutil.copyfile(path_to_file, saved_filename)
+            shutil.copyfile(path_to_file, saved_filename)
 
             print("[+] Found '%s' with version '%s'" % (filename, pev["FileVersion"]))
             if debug:
                 print("[+] Saved file to %s" % saved_filename)
 
-            #download_pdb(saved_filename, final_dir, debug=debug)
+            download_pdb(download_dir=final_dir, pathtopefile=saved_filename)
     except pefile.PEFormatError as e:
         pass
     return versions
